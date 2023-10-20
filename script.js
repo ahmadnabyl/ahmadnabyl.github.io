cc196b0c332bfb071890b12a0b1215babd1d28d1
AhmadNabil <147696638+ahmadnabyl@users.noreply.github.com>
2023-10-21 01:56:32 +0700

    step 3

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
+    
+    $('.profile-image').hover(function() {
+        $(this).attr('src', 'profile-1-hover.jpg');
+    }, function() {
+        $(this).attr('src', 'profile-1.jpg');
+    });
+    
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
