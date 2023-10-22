e7a7ff67a4eabc43ff358ba0d423b7e1581d328e
AhmadNabil <147696638+ahmadnabyl@users.noreply.github.com>
2023-10-22 18:47:15 +0700

    final step

--- script.js
+++ script.js
@@ -41,15 +41,38 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Gamer"],
+        strings: ["Student", "Gamer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Gamer"],
+        strings: ["Student", "Gamer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
